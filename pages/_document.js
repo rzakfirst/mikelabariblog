bec588c22fdc4914a8eb605060eab0dd2012e25e
Abdoul Razak <57506168+rzakfirst@users.noreply.github.com>
2022-03-18 17:40:37 +0100

    Header Buttun update

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8405458068605642"
      crossorigin="anonymous"></script>
           <title>Mi Ke Labari ?! </title>
@@ -18,8 +20,6 @@ class MyDocument extends Document {
           
         </Head>
         <body>
-          <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
           <Main />
           <NextScript />
         </body>
