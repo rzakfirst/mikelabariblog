993472913112493e2b4015b0ef01c868f39d70cc
Abdoul Razak <57506168+rzakfirst@users.noreply.github.com>
2022-03-18 17:21:51 +0100

    button

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,8 @@ class MyDocument extends Document {
           
         </Head>
         <body>
+          <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+          <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
           <Main />
           <NextScript />
         </body>
